e1df2b29a39bbadba0bb4917a063b41d604cd813
rewel03 <rewel.kibia@strathmore.edu>
2024-07-09 19:15:52 +0300

    q1

---
+++ 1_Hosp_q1.py
@@ -0,0 +1,23 @@
+patients = {
+    "Patinet 1" : {
+    "Name": "Samantha Boush",
+    "Age": "27",
+    "Illness": "Cold"
+    },
+
+    "Patient 2" : {
+     "Name": "Anthony Nick",
+     "Age": "50",
+    "Illness": "Joint Pains"
+    },
+
+    "Patient 3" : {
+     "Name": "Samantha Boush",
+     "Age": "27",
+      "Illness": "Cold"
+    }
+
+}
+
+
+
